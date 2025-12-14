a31ec3a9a08e125ee045d37f9ab9f8710a916ed0
Abil Shrestha <abilshr1@gmail.com>
2025-12-13 20:04:13 -0500

    Fix middleware response header setting for Vercel

--- application/middleware.ts
+++ application/middleware.ts
@@ -3,19 +3,18 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   if (request.method === "OPTIONS") {
-    const response = new NextResponse(null, { status: 204 });
-    response.headers.set("Access-Control-Allow-Origin", "*");
-    response.headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-    response.headers.set("Access-Control-Allow-Headers", "*");
-    return response;
+    const res = new NextResponse(null, { status: 204 });
+    res.headers.set("Access-Control-Allow-Origin", "*");
+    res.headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.headers.set("Access-Control-Allow-Headers", "*");
+    return res;
   }
-  return NextResponse.next({
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-      "Access-Control-Allow-Headers": "*",
-    },
-  });
+
+  const res = NextResponse.next();
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.headers.set("Access-Control-Allow-Headers", "*");
+  return res;
 }
 
 export const config = {
